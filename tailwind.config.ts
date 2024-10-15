b46482d7bd29f1ef0494669d5a785f7c047ce485
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-10-15 14:45:39 +0700

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -14,10 +15,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       boxShadow: {
-        'custom': '4px 7px 10px rgba(0, 0, 0, 0.4)',
-      }
+        custom: "4px 7px 10px rgba(0, 0, 0, 0.4)",
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
